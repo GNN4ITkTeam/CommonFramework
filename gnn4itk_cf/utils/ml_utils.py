667e83612c2d2748522ef353cc6759f255a64fc3
pmtuan <tuanpham1503@gmail.com>
2023-06-26 08:35:34 -0700

    add ml_utils

--- gnn4itk_cf/utils/ml_utils.py
+++ gnn4itk_cf/utils/ml_utils.py
@@ -22,6 +22,8 @@ def make_mlp(
     output_activation=None,
     layer_norm=False,
     batch_norm=False,
+    input_dropout=0,
+    hidden_dropout=0
 ):
     """Construct an MLP with specified fully-connected layers."""
     hidden_activation = getattr(nn, hidden_activation)
@@ -32,12 +34,16 @@ def make_mlp(
     sizes = [input_size] + sizes
     # Hidden layers
     for i in range(n_layers - 1):
+        if i==0 and input_dropout>0:
+            layers.append(nn.Dropout(input_dropout))
         layers.append(nn.Linear(sizes[i], sizes[i + 1]))
         if layer_norm:
             layers.append(nn.LayerNorm(sizes[i + 1], elementwise_affine=False))
         if batch_norm:
             layers.append(nn.BatchNorm1d(sizes[i + 1], track_running_stats=False, affine=False))
         layers.append(hidden_activation())
+        if hidden_dropout > 0:
+            layers.append(nn.Dropout(hidden_dropout))
     # Final layer
     layers.append(nn.Linear(sizes[-2], sizes[-1]))
     if output_activation is not None:
