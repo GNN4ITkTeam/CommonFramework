5c806932c16dc1b59fcfb25cd0f721433ea18dcb
sachin_atlas <sguptage1245@gmail.com>
2023-07-05 13:41:19 +0000

    removed bias from make_mlp

--- gnn4itk_cf/stages/graph_construction/models/utils.py
+++ gnn4itk_cf/stages/graph_construction/models/utils.py
@@ -149,14 +149,14 @@ def make_mlp(
     sizes = [input_size] + sizes
     # Hidden layers
     for i in range(n_layers - 1):
-        layers.append(nn.Linear(sizes[i], sizes[i + 1],bias = False))
+        layers.append(nn.Linear(sizes[i], sizes[i + 1]))
         if layer_norm:
             layers.append(nn.LayerNorm(sizes[i + 1], elementwise_affine=False))
         if batch_norm:
             layers.append(nn.BatchNorm1d(sizes[i + 1], track_running_stats=False, affine=False))
         layers.append(hidden_activation())
     # Final layer
-    layers.append(nn.Linear(sizes[-2], sizes[-1],bias = False))
+    layers.append(nn.Linear(sizes[-2], sizes[-1]))
     if output_activation is not None:
         if layer_norm:
             layers.append(nn.LayerNorm(sizes[-1], elementwise_affine=False))
