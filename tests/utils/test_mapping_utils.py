75b59a52a0de67229feaf3e0e19abb1e91caeedf
qwerasd903 <qwerasd903@me.com>
2023-12-12 12:25:17 -0800

    ci

--- tests/utils/test_mapping_utils.py
+++ tests/utils/test_mapping_utils.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 from acorn.utils.mapping_utils import (
-    infer_input_type,
+    get_variable_type,
     map_nodes_to_edges,
     map_nodes_to_tracks,
     map_tensor_handler,
@@ -14,40 +14,31 @@ def mock_data():
     num_edges = 20
     num_track_edges = 10
     edge_index = torch.randint(0, num_nodes, (2, num_edges))
-    x = torch.rand((num_nodes, 3))
-    truth_map = torch.randint(0, num_nodes, (num_edges,))
+    hit_x = torch.rand((num_nodes, 3))
+    # track_to_edge_map = torch.randint(0, num_nodes, (num_edges,))
     track_edges = torch.randint(0, num_nodes, (2, num_track_edges))
 
     return {
         "edge_index": edge_index,
-        "truth_map": truth_map,
-        "x": x,
+        # "track_to_edge_map": track_to_edge_map,
+        "hit_x": hit_x,
         "track_edges": track_edges,
     }
 
 
 def test_infer_input_type(mock_data):
-    input_tensor = mock_data["x"]
-    input_type, _ = infer_input_type(
-        input_tensor, num_nodes=10, num_edges=20, num_track_edges=10
-    )
+    input_type = get_variable_type("hit_x")
     assert input_type == "node-like"
 
-    input_tensor = mock_data["truth_map"]
-    input_type, _ = infer_input_type(
-        input_tensor, num_nodes=10, num_edges=20, num_track_edges=10
-    )
+    input_type = get_variable_type("edge_index")
     assert input_type == "edge-like"
 
-    input_tensor = mock_data["track_edges"]
-    input_type, _ = infer_input_type(
-        input_tensor, num_nodes=10, num_edges=20, num_track_edges=10
-    )
+    input_type = get_variable_type("track_edges")
     assert input_type == "track-like"
 
 
 def test_map_nodes_to_edges(mock_data):
-    nodelike_input = mock_data["x"]
+    nodelike_input = mock_data["hit_x"]
     edge_index = mock_data["edge_index"]
     result = map_nodes_to_edges(nodelike_input, edge_index, aggr="mean")
     assert result.shape == (20, 3)
@@ -63,7 +54,7 @@ def test_map_nodes_to_edges(mock_data):
 
 
 def test_map_nodes_to_tracks(mock_data):
-    nodelike_input = mock_data["x"]
+    nodelike_input = mock_data["hit_x"]
     track_edges = mock_data["track_edges"]
     result = map_nodes_to_tracks(nodelike_input, track_edges, aggr="mean")
     assert result.shape == (10, 3)
