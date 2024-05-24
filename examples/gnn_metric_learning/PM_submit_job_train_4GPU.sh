6976807495456ba1f1e2bc25825c44d3c2e7869e
qwerasd903 <qwerasd903@me.com>
2024-05-24 16:31:55 -0700

    small fix

--- examples/gnn_metric_learning/PM_submit_job_train_4GPU.sh
+++ examples/gnn_metric_learning/PM_submit_job_train_4GPU.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH -A m3443 -q regular
+#SBATCH -A m2616 -q regular
 #SBATCH -C gpu&hbm80g
 #SBATCH -t 12:00:00
 #SBATCH -N 1
@@ -11,7 +11,7 @@
 #SBATCH -J Acorn-train
 #SBATCH --gpu-bind=none
 #SBATCH --comment=96:00:00
-#SBATCH --signal=SIGUSR1@90
+#SBATCH --signal=SIGUSR1@300
 #SBATCH --requeue
 
 # This is a generic script for submitting training jobs to Cori-GPU.
@@ -22,9 +22,10 @@ mkdir -p logs
 eval "$(conda shell.bash hook)"
 
 # module load python/3.9-anaconda-2021.11
-conda activate acorn
+conda activate acorn_dev
 
 export SLURM_CPU_BIND="cores"
+export WANDB__SERVICE_WAIT=300
 echo -e "\nStarting training\n"
 
 # Single GPU training
