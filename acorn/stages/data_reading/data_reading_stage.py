5c5e8406a45d157dc4fe4f4ddca6ccec1ade4d95
Alexis Vallier <alexis.vallier@cern.ch>
2025-07-30 11:46:52 +0200

    cleanup + add possibility to run only on trainset for data reading stage

--- acorn/stages/data_reading/data_reading_stage.py
+++ acorn/stages/data_reading/data_reading_stage.py
@@ -670,43 +670,23 @@ class EventReader:
         all_files_in_template = []
         for files in files_per_template.values():
             all_files_in_template += files
-        # all_files_in_template = [
-        #     glob.glob(os.path.join(inputdir, f"*{template}*"))
-        #     for template in filename_terms
-        # ]
 
-        # all_files_in_template = list(chain.from_iterable(all_files_in_template))
         self.log.debug(f"Found {len(all_files_in_template)} files")
-        self.log.debug(
-            f"{all_files_in_template[0]}\n [...]\n{all_files_in_template[-1]}"
-        )
+        if len(all_files_in_template) > 0:
+            self.log.debug(
+                f"{all_files_in_template[0]}\n [...]\n{all_files_in_template[-1]}"
+            )
 
         self.log.debug("Find all event ids [0-9]+")
         all_event_ids = sorted(
             list({re.findall("[0-9]+", file)[-1] for file in all_files_in_template})
         )
         self.log.debug(f"Found {len(all_event_ids)} event IDs")
-        self.log.debug(f"{all_event_ids[:5]}\n [...]\n {all_event_ids[-5:]}")
+        if len(all_event_ids) > 0:
+            self.log.debug(f"{all_event_ids[:5]}\n [...]\n {all_event_ids[-5:]}")
 
         self.log.debug("Loop on all events ids")
         all_events = []
-        # for event_id in tqdm(all_event_ids):
-        #     event = {"event_id": event_id}
-        #     for term in filename_terms:
-        #         if template_file := [
-        #             file
-        #             for file in all_files_in_template
-        #             if term in os.path.basename(file)
-        #             and re.findall("[0-9]+", file)[-1] == event_id
-        #         ]:
-        #             event[term] = template_file[0]
-        #         else:
-        #             print(
-        #                 f"Could not find file for term {term} and event id {event_id}"
-        #             )
-        #             break
-        #     else:
-        #         all_events.append(event)
 
         dict_events = dict()
         for term in filename_terms:
@@ -728,7 +708,10 @@ class EventReader:
             d |= terms
             all_events.append(d)
 
-        self.log.debug(f"{all_events[:5]}\n [...]\n {all_events[-5:]}")
+        if len(all_events) > 0:
+            self.log.debug(f"{all_events[:5]}\n [...]\n {all_events[-5:]}")
+        else:
+            self.log.debug("No event files found")
 
         return all_events
 
@@ -739,7 +722,20 @@ class EventReader:
         return hits, particles, tracks
 
     def _test_csv_conversion(self):
-        for data_name in ["trainset", "valset", "testset"]:
+
+        # Handle cases when we do not provide the three categories train/valid/test in the yaml
+        input_sets = [
+            f"{dataset_name}set"
+            for dataset_name in ["train", "valid", "test"]
+            if dataset_name in self.config["input_sets"]
+        ]
+
+        # Fix validset / valset name mismatch
+        if "validset" in input_sets:
+            input_sets = [e for e in input_sets if e != "validset"]
+            input_sets.append("valset")
+
+        for data_name in input_sets:
             dataset = getattr(self, data_name)
             if dataset is None:
                 continue
@@ -766,27 +762,28 @@ class EventReader:
                 " conversion to CSV was successful."
             )
 
-        for dataset1, dataset2 in combinations(["trainset", "valset", "testset"], 2):
-            dataset1_files = {
-                event["event_id"]
-                for event in self.get_file_names(
-                    os.path.join(self.config["stage_dir"], dataset1),
-                    filename_terms=["truth", "particles"],
-                )
-            }
-            dataset2_files = {
-                event["event_id"]
-                for event in self.get_file_names(
-                    os.path.join(self.config["stage_dir"], dataset2),
-                    filename_terms=["truth", "particles"],
-                )
-            }
-            if dataset1_files.intersection(dataset2_files):
-                warnings.warn(
-                    f"There are overlapping files between the {dataset1} and"
-                    f" {dataset2}. You should remove these overlapping files from one"
-                    f" of the datasets: {dataset1_files.intersection(dataset2_files)}"
-                )
+        if len(input_sets) > 1:
+            for dataset1, dataset2 in combinations(input_sets, 2):
+                dataset1_files = {
+                    event["event_id"]
+                    for event in self.get_file_names(
+                        os.path.join(self.config["stage_dir"], dataset1),
+                        filename_terms=["truth", "particles"],
+                    )
+                }
+                dataset2_files = {
+                    event["event_id"]
+                    for event in self.get_file_names(
+                        os.path.join(self.config["stage_dir"], dataset2),
+                        filename_terms=["truth", "particles"],
+                    )
+                }
+                if dataset1_files.intersection(dataset2_files):
+                    warnings.warn(
+                        f"There are overlapping files between the {dataset1} and"
+                        f" {dataset2}. You should remove these overlapping files from one"
+                        f" of the datasets: {dataset1_files.intersection(dataset2_files)}"
+                    )
 
     def __len__(self):
         return len(self.files)
